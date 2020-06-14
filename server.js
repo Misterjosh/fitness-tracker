c55ad09c7bceb0b6ea4d7ed8e6e996775dcc7ea2
Josh Brooks <defaultjosh@hotmail.com>
2020-06-13 20:11:18 -0400

    Server.js complete

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
+//Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-//Start Server
+const PORT = process.env.PORT || 3000;
+
+//Setting up Express
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Setting up MONGO and Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+//Routes
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+//Start Server with clickable port link
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
 });
